c577680b4e7b10cb0ffc1f31dd85b34abe4fe4d7
isidor99 <sajinovic.isidor99@gmail.com>
2018-02-08 16:17:01 +0100

    Initial commit

---
+++ app/src/main/java/com/example/saki99/retrofit/RetrofitAppActivity.java
@@ -0,0 +1,13 @@
+package com.example.saki99.retrofit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RetrofitAppActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_retrofit_app);
+    }
+}
